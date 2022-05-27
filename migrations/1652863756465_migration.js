800f757982add2c36c46e4574544cb6aae5b083a
chipimo <chipimo31@gmail.com>
2022-05-27 09:29:57 +0200

    Database config

---
+++ migrations/1652863756465_migration.js
@@ -0,0 +1,57 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.createTable('customers', {
+        id: 'id',
+        fname: { type: 'varchar(1000)', notNull: true },
+        lname: { type: 'varchar(1000)', notNull: true },
+        dob: { type: 'varchar(1000)', notNull: true },
+        email: { type: 'varchar(1000)', notNull: true },
+        accountNumber: { type: 'varchar(1000)', notNull: true },
+        nrc: { type: 'varchar(1000)', notNull: true },
+        photoUri: { type: 'varchar(1000)', notNull: true },
+        password: { type: 'varchar(1000)', notNull: true },
+        isActive: { type: 'boolean', notNull: true },
+        createdAt: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+    })
+    pgm.createTable('account', {
+        id: 'id',
+        userId: {
+            type: 'integer',
+            notNull: true,
+            references: '"customers"',
+            onDelete: 'cascade',
+        },
+        balance: { type: 'numeric', notNull: true },
+        createdAt: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+    })
+    pgm.createTable('history', {
+        id: 'id',
+        userId: {
+            type: 'integer',
+            notNull: true,
+            references: '"customers"',
+            onDelete: 'cascade',
+        },
+        body: { type: 'text', notNull: true },
+        createdAt: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+    })
+    pgm.createIndex('account', 'userId')
+    pgm.createIndex('history', 'userId')
+}
+
+exports.down = pgm => { };
